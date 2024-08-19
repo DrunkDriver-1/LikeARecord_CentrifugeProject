cec8a4f0eff4ae088930bd8132b4bf79a3e8788c
DrunkDriver-1 <148510640+DrunkDriver-1@users.noreply.github.com>
2024-08-19 11:34:04 +1000

    improving Sensor code
    just a quick improvement to the sensor code, i changed out the counter function to use increments and there is now a time checker, it should also display rpm and RCF now

--- Code/HallEffectSensor/HallEffectSensor.ino
+++ Code/HallEffectSensor/HallEffectSensor.ino
@@ -5,6 +5,8 @@ volatile int rpm = 0;
 volatile int GForce = 0;
 bool isPulse = true;
 const int check = 20;
+int timmer = 0;
+int rotorRad = 20;
 void setup() {
   // put your setup code here, to run once:
   pinMode(sigPin, INPUT);
@@ -16,11 +18,27 @@ void loop() {
   //Serial.println(magVal);
   magVal = analogRead(sigPin);
   if (magVal >= check && isPulse == true){
-    Counter = Counter + 1;
+    Counter ++ ;
     isPulse = false;
+    timeCheck();
   }
   else{
     isPulse = true;
+    timeCheck();
   }
   Serial.println(Counter);
+};
+void timeCheck(){
+  timmer ++;
+  if (timmer >= 19200){
+    rpm = Counter * 30;
+    GForce = 11.2 * rotorRad * pow((rpm/1000),2);
+    Serial.println('the rpm of the rotor is currently ');
+    Serial.print(rpm);
+    Serial.println('and the RCF is ');
+    Serial.print(GForce);
+    Serial.print(' G');
+    Counter = 0;
+    timmer = 0;
+  }
 }
