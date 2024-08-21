d32314b8d028c586f803ac38ddb4f00b420a2bd5
DrunkDriver-1 <148510640+DrunkDriver-1@users.noreply.github.com>
2024-08-21 12:37:52 +1000

    Propper Quotations
    apparently i was using the wrong quotes for the prints, i was using '  where apparently i needed to ", i think it was being stored as a number. leading to one of the most confusing debugs i have ever been part of. now i just need to fix the math

--- Code/HallEffectSensor/HallEffectSensor.ino
+++ Code/HallEffectSensor/HallEffectSensor.ino
@@ -1,41 +1,46 @@
 const int sigPin = 6;
 int magVal = 0;
-volatile int Counter = 0;
-volatile int rpm = 0;
+float Counter = 1;
+float rpm = 1;
 volatile int GForce = 0;
 bool isPulse = true;
 const int check = 20;
 int timmer = 0;
 int rotorRad = 20;
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(sigPin, INPUT);
   Serial.begin(9600);
-  Serial.println('ok its started now');
+  Serial.println("ok its started now");
 }
+
+
 void timeCheck(){
-  timmer ++;
-  if (timmer >= 19200){
+  //timmer ++;
+  timmer = timmer +1;
+  if (timmer >= 19200) {
     rpm = Counter * 30;
     GForce = 11.2 * rotorRad * pow((rpm/1000),2);
-    Serial.println('the rpm of the rotor is currently ');
-    Serial.print(rpm);
-    Serial.println('and the RCF is ');
+    Serial.print("the rpm of the rotor is currently ");
+    Serial.println(rpm);
+    Serial.print("and the RCF is ");
     Serial.print(GForce);
-    Serial.print(' G');
+    Serial.println(" G");
     Counter = 0;
     timmer = 0;
   }
 }
+
+
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println(magVal);
   
  // magVal = analogRead(sigPin);
- magVal = digitalRead(sigPin);
-  noInterrupts();
+  magVal = digitalRead(sigPin);
   timeCheck();
-  interrupts();
   if (magVal == HIGH && isPulse == true){
 //   if (magVal >= check && isPulse == true){
     Counter ++ ;
