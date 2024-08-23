ea455f309f48b13f1f436aaa78335af8675c3ff4
DrunkDriver-1 <148510640+DrunkDriver-1@users.noreply.github.com>
2024-08-23 13:37:22 +1000

    Starting on the inputs and LCD
    now I'm starting to code the inputs, I'm starting with a rotary encoder to take the time and RCF inputs. its still not working but after it does work i will start on the LCD display

--- Code/BasicInputControl/BasicInputControl.ino
+++ Code/BasicInputControl/BasicInputControl.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+#define CLK 10  // Interrupt pin for CLK (A)
+#define DT 3   // Pin for DT (B)
+#define SW 4   // Interrupt pin for SW (Button)
 
+volatile int counter = 0;  // Counter for the number
+volatile int lastStateDT = 0;  // To keep track of the last state of DT
+volatile bool buttonPressed = false;  // Flag to track button press
+
+void setup() {
+  pinMode(CLK, INPUT_PULLUP);
+  pinMode(DT, INPUT_PULLUP);
+  pinMode(SW, INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+  // Initialize lastStateDT
+  lastStateDT = digitalRead(DT);
+
+  // Attach interrupt to CLK pin (triggers on a change in state)
+  attachInterrupt(digitalPinToInterrupt(CLK), updateEncoder, CHANGE);
+
+  // Attach interrupt to SW pin (triggers on button press)
+  attachInterrupt(digitalPinToInterrupt(SW), handleButtonPress, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Display the counter value
+  Serial.print("Counter: ");
+  Serial.println(counter);
+  delay(200);  // Just to slow down the serial output for readability
 
+  // Check if the button was pressed
+  if (buttonPressed) {
+    Serial.println("Button was pressed!");
+    buttonPressed = false;  // Reset the flag
+  }
+}
+
+void updateEncoder() {
+  // Read the state of DT
+  int currentStateDT = digitalRead(DT);
+
+  // Determine rotation direction
+  if (currentStateDT != lastStateDT) {
+    if (currentStateDT == HIGH) {
+      counter++;
+    } else {
+      counter--;
+    }
+  }
+
+  // Update last state of DT
+  lastStateDT = currentStateDT;
+}
+
+void handleButtonPress() {
+  // Set flag to indicate button press
+  buttonPressed = true;
+  
+  // Small delay to debounce button press (if needed)
+  delay(50);
 }
